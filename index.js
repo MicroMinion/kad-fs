2b88648bfd1b7e90f07a0050c99cd3bd3de155b5
Thomas Delaet <thomas@delaet.org>
2016-09-09 20:21:31 +0200

    standard formatting

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @module kad-fs
  */
 
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var assert = require('assert');
-var stream = require('readable-stream');
+var fs = require('fs')
+var assert = require('assert')
+var stream = require('readable-stream')
 var _ = require('lodash')
 
 /**
@@ -14,18 +14,18 @@ var _ = require('lodash')
  * @constructor
  * @param {String} datadir
  */
-function KadFSAdapter(datadir) {
+function KadFSAdapter (datadir) {
   if (!(this instanceof KadFSAdapter)) {
-    return new KadFSAdapter(datadir);
+    return new KadFSAdapter(datadir)
   }
 
-  this.datadir = datadir + '/';
+  this.datadir = datadir + '/'
 
   if (!fs.existsSync(datadir)) {
-    fs.mkdirSync(datadir);
+    fs.mkdirSync(datadir)
   }
 
-  assert(fs.statSync(datadir).isDirectory(), 'Invalid data directory');
+  assert(fs.statSync(datadir).isDirectory(), 'Invalid data directory')
 }
 
 /**
@@ -34,24 +34,24 @@ function KadFSAdapter(datadir) {
  * @param {String} key
  * @param {Function} callback
  */
-KadFSAdapter.prototype.get = function(key, callback) {
+KadFSAdapter.prototype.get = function (key, callback) {
   assert(_.isString(key), 'key is not a valid string')
   assert(_.isFunction(callback), 'callback is not a valid function')
-  var self = this;
-  key = new Buffer(key, 'utf8').toString('base64');
-  fs.exists(self.datadir + key, function(exists) {
+  var self = this
+  key = new Buffer(key, 'utf8').toString('base64')
+  fs.exists(self.datadir + key, function (exists) {
     if (!exists) {
-      return callback(new Error('Not found'));
+      return callback(new Error('Not found'))
     }
 
-    fs.readFile(self.datadir + key, function(err, contents) {
+    fs.readFile(self.datadir + key, function (err, contents) {
       if (err) {
-        return callback(err);
+        return callback(err)
       }
-      callback(null, contents.toString());
-    });
-  });
-};
+      callback(null, contents.toString())
+    })
+  })
+}
 
 /**
  * Places the value at a file named key
@@ -60,13 +60,13 @@ KadFSAdapter.prototype.get = function(key, callback) {
  * @param {String} value
  * @param {Function} callback
  */
-KadFSAdapter.prototype.put = function(key, value, callback) {
+KadFSAdapter.prototype.put = function (key, value, callback) {
   assert(_.isString(key), 'key is not a valid string')
   assert(!callback || _.isFunction(callback), 'callback is not a valid function')
   assert(_.isString(value), 'value is not a valid string')
-  key = new Buffer(key, 'utf8').toString('base64');
-  fs.writeFile(this.datadir + key, value, callback);
-};
+  key = new Buffer(key, 'utf8').toString('base64')
+  fs.writeFile(this.datadir + key, value, callback)
+}
 
 /**
  * Deletes the value at the given key
@@ -74,51 +74,51 @@ KadFSAdapter.prototype.put = function(key, value, callback) {
  * @param {String} key
  * @param {Function} callback
  */
-KadFSAdapter.prototype.del = function(key, callback) {
+KadFSAdapter.prototype.del = function (key, callback) {
   assert(_.isString(key), 'key is not a valid string')
   assert(!callback || _.isFunction(callback), 'callback is not a valid function')
-  var self = this;
-  key = new Buffer(key, 'utf8').toString('base64');
+  var self = this
+  key = new Buffer(key, 'utf8').toString('base64')
 
-  fs.exists(self.datadir + key, function(exists) {
+  fs.exists(self.datadir + key, function (exists) {
     if (!exists) {
-      return callback();
+      return callback()
     }
 
-    fs.unlink(self.datadir + key, callback);
-  });
-};
+    fs.unlink(self.datadir + key, callback)
+  })
+}
 
 /**
  * Retruns a stream of the entire database
  * #createReadStream
  */
-KadFSAdapter.prototype.createReadStream = function() {
-  var adapter = this;
-  var items = fs.readdirSync(this.datadir);
-  var current = 0;
+KadFSAdapter.prototype.createReadStream = function () {
+  var adapter = this
+  var items = fs.readdirSync(this.datadir)
+  var current = 0
 
   return new stream.Readable({
     objectMode: true,
-    read: function() {
-      var stream = this;
-      var key = items[current];
+    read: function () {
+      var stream = this
+      var key = items[current]
 
       if (!key) {
-        return stream.push(null);
+        return stream.push(null)
       }
-      key = new Buffer(key, 'utf8').toString('base64');
+      key = new Buffer(key, 'utf8').toString('base64')
 
-      fs.readFile(adapter.datadir + key, function(err, contents) {
+      fs.readFile(adapter.datadir + key, function (err, contents) {
         if (err) {
-          return stream.emit('error', err);
+          return stream.emit('error', err)
         }
 
-        current++;
-        stream.push({ key: key, value: contents.toString() });
-      });
+        current++
+        stream.push({ key: key, value: contents.toString() })
+      })
     }
-  });
-};
+  })
+}
 
-module.exports = KadFSAdapter;
+module.exports = KadFSAdapter
